ea00f9583b4ecdadf34127b2345cf57b8944fb8f
Bohdan Akimenko <Bohdan_Akimenko@epam.com>
2021-08-18 19:07:25 +0300

    Kotlin synthetic was replaced by View Bindings.

--- app/src/main/java/com/user576/tonometerrecorder/AddRecordActivity.kt
+++ app/src/main/java/com/user576/tonometerrecorder/AddRecordActivity.kt
@@ -2,39 +2,47 @@ package com.user576.tonometerrecorder
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.add_layout.add_btn
-import kotlinx.android.synthetic.main.add_layout.sys
-import kotlinx.android.synthetic.main.add_layout.dia
-import kotlinx.android.synthetic.main.add_layout.pulse
+import com.user576.tonometerrecorder.databinding.AddLayoutBinding
 
 class AddRecordActivity : AppCompatActivity() {
+
+    private lateinit var binding : AddLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.add_layout)
+        binding = AddLayoutBinding.inflate(layoutInflater)
 
-        add_btn.setOnClickListener {
-            val sysStr : String = sys.text.toString()
-            val diaStr : String = dia.text.toString()
-            val pulseStr : String = pulse.text.toString()
+        setContentView(binding.root)
+    }
 
-            val result = Record.verifyParamsForCreate(sysStr, diaStr, pulseStr)
-            if (result == Record.VALID_RES) {
-                val record = Record.create(sysStr, diaStr, pulseStr)
-                RecorderApp.dao.insert(record)
-                startActivity(
-                    Intent(this, MainActivity::class.java)
-                )
-                finish()
-            } else {
-                AlertDialog.Builder(it.context)
-                    .setMessage("Значение $result не корректно")
-                    .show()
-            }
+    fun onBtnClick(view : View) {
+
+        val sysStr : String
+        val diaStr : String
+        val pulseStr : String
+
+        with(binding) {
+            sysStr = sys.text.toString()
+            diaStr = dia.text.toString()
+            pulseStr = pulse.text.toString()
+        }
+
+        val result = Record.verifyParamsForCreate(sysStr, diaStr, pulseStr)
+        if (result == Record.VALID_RES) {
+            val record = Record.create(sysStr, diaStr, pulseStr)
+            RecorderApp.dao.insert(record)
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+            finish()
+        } else {
+            AlertDialog.Builder(view.context)
+                .setMessage("Значение $result не корректно")
+                .show()
         }
     }
 }
--- app/src/main/java/com/user576/tonometerrecorder/MainActivity.kt
+++ app/src/main/java/com/user576/tonometerrecorder/MainActivity.kt
@@ -2,32 +2,47 @@ package com.user576.tonometerrecorder
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.main_layout.table_layout
-import kotlinx.android.synthetic.main.main_layout.add_record_btn
+import com.user576.tonometerrecorder.databinding.MainLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : MainLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.main_layout)
+        binding = MainLayoutBinding.inflate(layoutInflater)
 
-        add_record_btn.setOnClickListener {
-            startActivity(
-                Intent(this, AddRecordActivity::class.java)
-            )
-            finish()
+        with (binding.header){
+            date.text = resources.getString(R.string.th_date)
+            sys.text = resources.getString(R.string.th_sys)
+            dia.text = resources.getString(R.string.th_dia)
+            pulse.text = resources.getString(R.string.th_pulse)
         }
 
+        setContentView(binding.root)
+
         RecorderApp.appContext = this.applicationContext
+/*
         table_layout.setRecords(
             RecorderApp.dao.getAll()
         )
+*/
     }
 
     override fun onDestroy() {
         resetRowsNumber()
         super.onDestroy()
+    }
+
+    fun onBtnClick(view : View) {
+        if (view.id == R.id.add_record_btn) {
+            startActivity(
+                Intent(this, AddRecordActivity::class.java)
+            )
+            finish()
+        }
     }
 }
